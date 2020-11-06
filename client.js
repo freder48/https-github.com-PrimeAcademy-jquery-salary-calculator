4f6eaa2984a68c8c4102379acc8a666537b91128
Josie Fredericksen <jfredericksen12@gmail.com>
2020-11-06 16:29:39 -0600

    Added Input Boxes & Button

--- client.js
+++ client.js
@@ -1,7 +1,27 @@
+let employee = [];
 console.log('Hello');
 
 $(document).ready(readyNow); 
 
 function readyNow(){
     console.log('Hello from JQ');
+    $('#submit').on('click', addEmployee);
+
+}
+
+function addEmployee (event){
+console.log('Adding Employee');
+event.preventDefault();
+
+let firstName = $('.firstInput').val();
+console.log(firstName);
+let lastName = $('.lastInput').val();
+console.log(lastName);
+let idInput = $('.idInput').val();
+console.log(idInput);
+let titleInput = $('.titleInput').val();
+console.log(titleInput);
+let salaryInput = $('.salaryInput').val();
+console.log(salaryInput);
+
 }
